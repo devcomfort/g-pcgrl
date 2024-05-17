1b217622a0802eb3cfe9f4f847e30e91933c4790
Florian Rupp <rupp@hdm-stuttgart.de>
2024-05-17 16:05:57 +0200

    Code cleaning

--- gym_pcgrl/envs/reps/wide_graph.py
+++ gym_pcgrl/envs/reps/wide_graph.py
@@ -1,10 +1,8 @@
-from gym_pcgrl.envs.reps.representation import Representation
-from PIL import Image
-from gymnasium import spaces
 import numpy as np
-from gym_pcgrl.envs.helper import gen_preset_random_map, gen_random_map
-from gym_pcgrl.envs.reps.narrow_graph import triangular, triangular_root
+from gymnasium import spaces
 
+from gym_pcgrl.envs.reps.narrow_graph import triangular, triangular_root
+from gym_pcgrl.envs.reps.representation import Representation
 
 """
 The wide representation where the agent can pick the tile position and tile value at each update.
@@ -21,8 +19,8 @@ class WideGraphRepresentation(Representation):
         self.init_random_map = kwargs["init_random_map"]
         self.width = kwargs["width"]
         self.height = kwargs["height"]
-        self.size = triangular(self.height-1)
-        
+        self.size = triangular(self.height - 1)
+
     def get_action_space(self, width, height, num_tiles):
         return spaces.MultiDiscrete([self.size, 2])
 
@@ -31,60 +29,53 @@ class WideGraphRepresentation(Representation):
             "map": spaces.Box(low=0, high=num_tiles - 1, dtype=np.uint8, shape=(height, width))
         })
 
-
     def get_observation(self):
         return {
             "map": self._map.copy()
         }
-    
+
     def update4(self, action):
+        # update function 2,3, and 4 are experimental and currently not used
         # predict change at predicted position then swap value 0/1
-        #print(action)
         pos = action[0] + 1
-        tile = action[1] # 0 or 1
+        tile = action[1]  # 0 or 1
         y = triangular_root(pos) - 1
         x = pos - triangular(y) - 1
         y += 1
-        
+
         if self._map[y][x] == tile:
             return False, x, y
         else:
             self._map[y][x] = tile
             return True, x, y
 
-    
     def update(self, action):
         # predict change at predicted position then swap value 0/1
-        #print(action)
         pos = action[0] + 1
-        replace = action[1] # 0 or 1
+        replace = action[1]  # 0 or 1
         y = triangular_root(pos) - 1
         x = pos - triangular(y) - 1
         y += 1
-        
-        if replace == 1: # just flip
+
+        if replace == 1:  # just flip
             if self._map[y][x] == 1:
                 self._map[y][x] = 0
             else:
                 self._map[y][x] = 1
-                #print("Changing to 1 at pos", y, x)
-                #print(self._map)
             return True, x, y
         else:
             return False, x, y
-    
-    
+
     def update3(self, action):
+        # experimental
         # predict change at predicted position with predicted tile
         pos = action[0] + 1
         tile = action[1]
-        replace = action[2] # 0 or 1
+        replace = action[2]  # 0 or 1
         y = triangular_root(pos) - 1
         x = pos - triangular(y) - 1
         y += 1
-        
-        #print("action", action, "x", x, "y", y)
-        
+
         if self._map[y][x] == tile:
             return False, x, y
         elif replace == 1:
@@ -92,15 +83,15 @@ class WideGraphRepresentation(Representation):
             return True, x, y
         else:
             return False, x, y
-    
-    
+
     def update2(self, action):
+        # experimental
         pos = action[0] + 1
         tile = action[1]
-        replace = action[2] # 0 or 1
+        replace = action[2]  # 0 or 1
         x = triangular_root(pos) - 1
-        y = pos - triangular(x) - 1  
-        
+        y = pos - triangular(x) - 1
+
         if self._map[y][x] == tile:
             return False, x, y
         else:
--- gym_pcgrl/wrappers_graph.py
+++ gym_pcgrl/wrappers_graph.py
@@ -1,8 +1,8 @@
-import numpy as np
 import gymnasium as gym
+import numpy as np
 from gymnasium.core import Wrapper
 
-from gym_pcgrl.envs.reps.narrow_graph import triangular, triangular_root
+from gym_pcgrl.envs.reps.narrow_graph import triangular
 from gym_pcgrl.wrappers import OneHotEncoding
 
 
@@ -11,6 +11,7 @@ class OneHotGraph(OneHotEncoding):
         super().__init__(game, name, **kwargs)
         self.dim = kwargs["node_types"]
 
+
 class GraphWrapper(Wrapper):
     # use with graph-wide representation
     def __init__(self, game, **kwargs):
@@ -18,32 +19,28 @@ class GraphWrapper(Wrapper):
             self.env = gym.make(game, **kwargs)
         else:
             self.env = game
-        #get_pcgrl_env(self.env).adjust_param(**kwargs)
         self.env = OneHotGraph(self.env, 'map', **kwargs)
         self.width = kwargs["width"]
         self.size = triangular(self.width)
         self.num_tiles = kwargs["node_types"]
         gym.Wrapper.__init__(self, self.env)
-        self.observation_space = gym.spaces.Box(low=0, high=1, shape=[self.width, self.width, self.num_tiles], dtype=np.float32)
-
+        self.observation_space = gym.spaces.Box(low=0, high=1, shape=[self.width, self.width, self.num_tiles],
+                                                dtype=np.float32)
 
     def step(self, action):
-        #pos, replace = np.unravel_index(action, (self.size, 2)) # size, num_tiles, replace 0/1       
-        #obs, reward, done, truncated, info = self.env.step([pos, replace])
         obs, reward, done, truncated, info = self.env.step(action)
         obs = self.transform(obs)
         return obs, reward, done, truncated, info
-    
+
     def reset(self, seed=None, options=None):
         obs, stats = self.env.reset()
         obs = self.transform(obs)
         return obs, stats
-        
+
     def transform(self, obs):
-        return obs[self.name] #* 255
-        #return th.tensor(obs[self.name]).to(dtype=th.float32) * 255 #.to(torch.device("cuda"))
-        
-        
+        return obs[self.name]  # * 255
+
+
 class GraphNarrowWrapper(GraphWrapper):
     # use with graph-wide representation
     def __init__(self, game, **kwargs):
@@ -51,7 +48,6 @@ class GraphNarrowWrapper(GraphWrapper):
             self.env = gym.make(game, **kwargs)
         else:
             self.env = game
-        #get_pcgrl_env(self.env).adjust_param(**kwargs)
         self.env = OneHotGraph(self.env, 'map', **kwargs)
         self.width = kwargs["width"]
         self.size = triangular(self.width)
@@ -59,29 +55,21 @@ class GraphNarrowWrapper(GraphWrapper):
         gym.Wrapper.__init__(self, self.env)
         self.observation_space = gym.spaces.Box(low=0, high=1, shape=[2, self.width, self.num_tiles], dtype=np.float32)
 
-
     def step(self, action):
-        #pos, replace = np.unravel_index(action, (self.size, 2)) # size, num_tiles, replace 0/1       
-        #obs, reward, done, truncated, info = self.env.step([pos, replace])
         obs, reward, done, truncated, info = self.env.step(action)
         obs = self.transform(obs)
         return obs, reward, done, truncated, info
-    
+
     def reset(self, seed=None, options=None):
         obs, stats = self.env.reset()
         obs = self.transform(obs)
         return obs, stats
-        
+
     def transform(self, obs):
-        #print(obs)
         m = obs["map"].copy()
-        #print(m)
         y, x = obs["pos"][0], obs["pos"][1]
-        # select col/row "around the corner" of a-matrix
         row = m[y]
-        row[y+1:] = m[:, y][y+1:]
+        row[y + 1:] = m[:, y][y + 1:]
         col = m[x]
-        col[x+1:] = m[:, x][x+1:]
-        #print(row, col)
+        col[x + 1:] = m[:, x][x + 1:]
         return np.array([row, col])
-        #return th.tensor(np.array([row, col])).to(dtype=th.float32) #* 255
